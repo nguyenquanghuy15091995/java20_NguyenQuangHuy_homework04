4a79d4eba5d51fd958433084d5e9240d756877fd
Huy Edward Nguyen <nguyenquanghuy15091995@gmail.com>
2022-12-21 22:46:31 +0700

    Initial commit

---
+++ sql_bookstore.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE db_bookstore;
+
+-- DROP DATABASE db_bookstore;
+
+CREATE TABLE tb_customer (
+    cus_id int auto_increment,
+	cus_name nvarchar(255),
+    cus_birthday date,
+    cus_sex bool,
+    cus_phone varchar(50),
+    cus_email nvarchar(200),
+    cus_address nvarchar(2000),
+    cus_account varchar(1000),
+    cus_password varchar(1000),
+    PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE tb_author (
+	aut_id int auto_increment,
+    aut_name nvarchar(255),
+    aut_phone varchar(50),
+    aut_address nvarchar(2000),
+    aut_history text,
+    PRIMARY KEY (aut_id)
+);
+
+CREATE TABLE tb_publisher (
+	publ_id int auto_increment,
+    publ_name nvarchar(255),
+    publ_phone varchar(50),
+    publ_address nvarchar(2000),
+    PRIMARY KEY (publ_id)
+);
